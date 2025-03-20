b87afa2f6445256c19fe0dfefb40656b694e4dd2
Marco Villano <91207733+Marco010404@users.noreply.github.com>
2025-03-20 18:42:06 +0100

    Add files via upload

---
+++ Figura/src/main/java/com/mycompany/figura/Figura.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.figura;
+
+/**
+ *
+ * @author marcovillano
+ */
+
+public interface Figura{
+    
+    //restituisce l'area della figura
+    double area();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
